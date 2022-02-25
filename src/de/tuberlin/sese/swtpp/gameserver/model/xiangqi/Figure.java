aab944883a90ff4a0c3bdf195d752e59a454ba9d
JoshivP <joshiv18122001@gmail.com>
2022-02-25 20:09:12 +0100

    removeFromCheckables() wurde vorher entfernt
    - Beim Mergen wurde komischerweise
    removeFromCheckables() entfernt
    - Ist jetzt wieder drinnen
    - Find aber TryMoveIntegration() immernoch sus
    - Am besten mal anschauen

--- src/de/tuberlin/sese/swtpp/gameserver/model/xiangqi/Figure.java
+++ src/de/tuberlin/sese/swtpp/gameserver/model/xiangqi/Figure.java
@@ -43,6 +43,14 @@ public abstract class Figure implements Serializable{
 		}
 	}
 	
+	public void removeFromCheckable() {
+		if(!color) {
+			board.redFigsCheckable.remove(this);
+		}
+		else {
+			board.blackFigsCheckable.remove(this);
+		}
+	}
 	
 	public void setPosition(int[] pos) {
 		getBoard().setBoardEntry(this.pos, null);
